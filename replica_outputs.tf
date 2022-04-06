3c921f42e56a08964a7ba47ad1a2a1afbfeaff8d
Henry He <henry.he@betterworks.com>
2022-04-06 16:21:18 -0600

    fix: same types on output

--- replica_outputs.tf
+++ replica_outputs.tf
@@ -1,14 +1,14 @@
 output "replica_instance_id" {
-  value       = aws_db_instance.replica.*.id
+  value       = var.enabled && var.replica_count > 0 ? aws_db_instance.replica.*.id : []
   description = "ID of the replica instances"
 }
 
 output "replica_instance_address" {
-  value       = aws_db_instance.replica.*.address
+  value       = var.enabled && var.replica_count > 0 ? aws_db_instance.replica.*.address : []
   description = "Addresses of the replica instances"
 }
 
 output "replica_instance_endpoint" {
-  value       = aws_db_instance.replica.*.endpoint
+  value       = var.enabled && var.replica_count > 0 ? aws_db_instance.replica.*.endpoint : []
   description = "DNS Endpoint of the replica instances"
 }
