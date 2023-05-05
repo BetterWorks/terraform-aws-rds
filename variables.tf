6b9bb61769b5a6b70788a2187c16e29a5c8d2b0c
Henry He <henry.he@betterworks.com>
2023-05-05 15:17:36 -0600

    fix: monitoring interval

--- variables.tf
+++ variables.tf
@@ -270,7 +270,7 @@ variable "kms_key_arn" {
 
 variable "monitoring_interval" {
   description = "The interval in seconds enhanced monitoring metrics are sent to cloudwatch"
-  default     = 25
+  default     = 30
 }
 
 variable "monitoring_role_arn" {
